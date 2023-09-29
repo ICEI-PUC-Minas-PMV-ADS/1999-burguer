15c6d69b69fd6ca8a4e2f7582745f99a76ffe049
Gabriel Puddo <gabriel@regexsolutions.com.br>
2023-09-28 21:59:54 -0300

    https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2023-2-e4-proj-dad-t3-1999-burguer/issues/30 retorna detalhes pedido

--- src/api/src/server/controllers/pedidos/GetByID.ts
+++ src/api/src/server/controllers/pedidos/GetByID.ts
@@ -1,21 +1,68 @@
 import { Request, RequestHandler, Response } from 'express';
 import { validation } from '../../shared/middleware';
-import * as YUP from 'yup'
-import { StatusCodes } from 'http-status-codes'
+import * as YUP from 'yup';
+import { StatusCodes } from 'http-status-codes';
+import { PedidosProvider } from '../../database/providers/pedidos';
 
 interface IParamProps {
     id?: number
 }
 
-export const getOrderByIdValidation: RequestHandler = validation((getSchema) => ({
+export const getPedidoByIdValidation: RequestHandler = validation((getSchema) => ({
     params: getSchema<IParamProps>(
         YUP.object().shape({
-            id: YUP.number().integer().required().moreThan(0),
+            id: YUP.number().integer().required().moreThan(0)
         })
-    ),
+    )
 }))
 
-export const getOrderById = async (
-    request: Request<IParamProps>,
-    response: Response
-) => {}
+export const getPedidoById = async (
+    req: Request<IParamProps>,
+    res: Response
+) => {
+
+    const params = req.params;
+
+    if (!params) {
+
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .json({
+                errors: {
+                    default: 'Parâmetros não informados.'
+                }
+            });
+
+    }
+
+    if (!params.id) {
+
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .json({
+                errors: {
+                    default: 'Pedido não informado.'
+                }
+            });
+
+    }
+
+    let result = PedidosProvider.getById(params.id);
+
+    if (result instanceof Error) {
+
+        return res
+            .status(StatusCodes.INTERNAL_SERVER_ERROR)
+            .json({
+                errors: {
+                    default: result.message
+                }
+            });
+
+    }
+
+    return res
+        .status(StatusCodes.OK)
+        .json(result);
+
+}
