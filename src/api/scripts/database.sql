d5e66818275390f4a987a65d12a4e02e6f601083
João Lessa <joaovictorlessa98@gmail.com>
2023-10-06 17:32:44 -0300

    Scripts padrões database

--- src/api/scripts/database.sql
+++ src/api/scripts/database.sql
@@ -65,5 +65,35 @@ CREATE TABLE teste.pedido_produto (
 INSERT INTO teste.loja (id, nome, horario_abertura, horario_fechamento)
 VALUES (1, 'Loja Matriz', '18:00', '00:00');
 
-INSERT INTO teste.usuario ( id, nome, email, senha )
-VALUES (1, 'admin', 'admin@admin.com', '$2a$08$vxHO36vOo.aJ5J3dR.qA3O73/7cDIJQ7ZWlyhjMx/tvTV.EwFTmky');
+/*Usuarios teste*/
+
+INSERT INTO teste.usuario ( id, email, senha, nome, endereco, numero, bairro, cidade, cep, uf, ponto_referencia, telefone, funcionario )
+VALUES (1, 'admin@admin.com', '$2a$08$vxHO36vOo.aJ5J3dR.qA3O73/7cDIJQ7ZWlyhjMx/tvTV.EwFTmky', 'admin', 'Rua Nilza Brito', '51', 'vitória', 'Belo Horizonte', '31970755', 'MG', 'praça do ipê', '31988886666', false);
+
+INSERT INTO teste.usuario ( id, email, senha, nome, endereco, numero, bairro, cidade, cep, uf, ponto_referencia, telefone, funcionario )
+VALUES (2, 'user1@teste.com', '$2a$08$vxHO36vOo.aJ5J3dR.qA3O73/7cDIJQ7ZWlyhjMx/tvTV.EwFTmky', 'user1', 'Rua Nilza Brito', '51', 'vitória', 'Belo Horizonte', '31970755', 'MG', 'praça do ipê', '31988886666', false);
+
+INSERT INTO teste.usuario ( id, email, senha, nome, endereco, numero, bairro, cidade, cep, uf, ponto_referencia, telefone, funcionario )
+VALUES (3, 'user2@teste.com', '$2a$08$vxHO36vOo.aJ5J3dR.qA3O73/7cDIJQ7ZWlyhjMx/tvTV.EwFTmky', 'user2', 'Rua Nilza Brito', '51', 'vitória', 'Belo Horizonte', '31970755', 'MG', 'praça do ipê', '31988886666', false);
+
+
+/*Produtos*/
+INSERT INTO teste.produto (id, descricao, valor, nome, status) VALUES (1, 'carne, alface, tomate, ovo, pão, batata palha', 24.80, 'X-burger', true);
+INSERT INTO teste.produto (id, descricao, valor, nome, status) VALUES (2, 'carne de frang, alface, tomate, ovo, pão, batata palha', 21.80, 'X-Frango', true);
+INSERT INTO teste.produto (id, descricao, valor, nome, status) VALUES (3, 'carne de picanha, alface, tomate, ovo, pão, batata palha', 29.90, 'X-Picanha', true);
+INSERT INTO teste.produto (id, descricao, valor, nome, status) VALUES (4, 'pão, ovo, presunto, mussarela', 10.00, 'Misto Quente', true);
+INSERT INTO teste.produto (id, descricao, valor, nome, status) VALUES (5, 'queijo brie, alface, tomate, ovo, pão, batata palha', 24.80, 'X-queijo', true);
+INSERT INTO teste.produto (id, descricao, valor, nome, status) VALUES (6, 'tofu, alface, tomate, pão vegano, batata palha', 35.00, 'X-vegano', true);
+
+
+/*Pedidos*/
+
+INSERT INTO teste.pedido (id, usuario_id, total, endereco, numero, bairro, cidade, cep, uf, status, data_finalizacao) VALUES (1, 3, 24.80, 'Rua nilza brito', '51', 'vitoria', 'belo horizonte', '31970755', 'MG',true, TO_TIMESTAMP(1633530000));
+INSERT INTO teste.pedido (id, usuario_id, total, endereco, numero, bairro, cidade, cep, uf, status, data_finalizacao) VALUES (2, 3, 40.90, 'Rua nilza brito', '51', 'vitoria', 'belo horizonte', '31970755', 'MG',true, TO_TIMESTAMP(1633530000));
+INSERT INTO teste.pedido (id, usuario_id, total, endereco, numero, bairro, cidade, cep, uf, status, data_finalizacao) VALUES (3, 2, 35.00, 'Rua nilza brito', '51', 'vitoria', 'belo horizonte', '31970755', 'MG', true, TO_TIMESTAMP(1633530000));
+
+
+/*Pedido Produto*/
+
+INSERT INTO pedido_produto (id, pedido_id, produto_id, quantidade, valor_unitario, total) VALUES (1, 1, 2, 2, 21.80, 53.60);
+
