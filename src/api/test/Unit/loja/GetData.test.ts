a16f237a3fedcf2d41648dc9ac63374631bd4e7a
João Lessa <joaovictorlessa98@gmail.com>
2023-10-06 17:33:20 -0300

    Atualização autenticação testes loja

--- src/api/test/Unit/loja/GetData.test.ts
+++ src/api/test/Unit/loja/GetData.test.ts
@@ -5,9 +5,30 @@ import { StatusCodes } from 'http-status-codes'
 
 describe('Loja - GetData', () => {
 
+    let accessToken = ''
+    beforeAll(async()=>{
+        const signInResponse = await testServer.post('/login').send({
+            email: 'admin@admin.com',
+            senha:'administrator'
+
+        })
+
+        accessToken = signInResponse.body.accessToken
+    });
+
+    it('Tenta resgatar informações sem autenticação',async ( ) => {
+        const output = await testServer
+            .get('/store-data/1')
+
+
+        expect(output.statusCode).toEqual(StatusCodes.UNAUTHORIZED);
+        expect(typeof output.body).toEqual('object');
+    })
+
     it('Tenta resgatar informações da loja matriz', async() => {
         const output = await testServer
             .get('/store-data/1')
+            .set('Authorization', `Bearer ${accessToken}`)
             .send()
         expect(output.statusCode).toEqual(StatusCodes.OK)
         expect(output.body).toHaveProperty('id');
@@ -19,6 +40,7 @@ describe('Loja - GetData', () => {
     it('Tenta resgatar informações da loja não existente', async() => {
         const output = await testServer
             .get('/store-data/3')
+            .set('Authorization', `Bearer ${accessToken}`)
             .send()
 
         expect(output.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR)
@@ -28,6 +50,7 @@ describe('Loja - GetData', () => {
     it('Tenta resgatar informações da loja passando um texto', async () => {
         const output = await testServer
             .get('/store-data/loja-matriz')
+            .set('Authorization', `Bearer ${accessToken}`)
             .send()
         expect(output.statusCode).toEqual(StatusCodes.BAD_REQUEST)
         expect(output.body).toHaveProperty('errors.params.id')
--- src/api/test/Unit/loja/UpdateByID.test.ts
+++ src/api/test/Unit/loja/UpdateByID.test.ts
@@ -4,9 +4,34 @@ import { StatusCodes } from 'http-status-codes'
 
 
 describe('Loja - Update', () => {
+
+    let accessToken = ''
+    beforeAll(async()=>{
+        const signInResponse = await testServer.post('/login').send({
+            email: 'admin@admin.com',
+            senha:'administrator'
+
+        })
+
+        accessToken = signInResponse.body.accessToken
+    });
+
+    it('Tenta atualizar informações sem autenticação',async ( ) => {
+        const output = await testServer
+            .get('/store-data/1')
+            .send({nome: 'Loja testes update'})
+
+
+        expect(output.statusCode).toEqual(StatusCodes.UNAUTHORIZED);
+        expect(typeof output.body).toEqual('object');
+    });
+
+
+
     it('Deve atualizar o nome de um registro de loja', async () => {
         const output = await testServer
             .put('/store-data/1')
+            .set('Authorization', `Bearer ${accessToken}`)
             .send({ nome: '1999 Burguer - Marajó' })
 
         expect(output.statusCode).toEqual(StatusCodes.ACCEPTED)
@@ -16,6 +41,7 @@ describe('Loja - Update', () => {
     it('Deve atualizar o horario de abertura de um registro de loja', async () => {
         const output = await testServer
             .put('/store-data/1')
+            .set('Authorization', `Bearer ${accessToken}`)
             .send({ horario_abertura: '19:30' })
 
         expect(output.statusCode).toEqual(StatusCodes.ACCEPTED)
@@ -25,6 +51,7 @@ describe('Loja - Update', () => {
     it('Deve atualizar o horario de fechamento de um registro de loja', async () => {
         const output = await testServer
             .put('/store-data/1')
+            .set('Authorization', `Bearer ${accessToken}`)
             .send({ horario_fechamento: '21:45' })
 
         expect(output.statusCode).toEqual(StatusCodes.ACCEPTED)
@@ -34,6 +61,7 @@ describe('Loja - Update', () => {
     it('Deve atualizar um registro de loja passando os três parâmetros', async () => {
         const output = await testServer
             .put('/store-data/1')
+            .set('Authorization', `Bearer ${accessToken}`)
             .send({ nome: '1999 Burguer - Matriz',
                 horario_abertura: '20:30',
                 horario_fechamento: '02:30'
@@ -46,6 +74,7 @@ describe('Loja - Update', () => {
     it('Tenta atualizar um registro de loja com nome muito curto', async () => {
         const output = await testServer
             .put('/store-data/1')
+            .set('Authorization', `Bearer ${accessToken}`)
             .send({ nome: '19' })
 
         expect(output.statusCode).toEqual(StatusCodes.BAD_REQUEST)
@@ -55,6 +84,7 @@ describe('Loja - Update', () => {
     it('Tenta criar um registro de loja onde nome é uma string vazia', async () => {
         const output = await testServer
             .put('/store-data/1')
+            .set('Authorization', `Bearer ${accessToken}`)
             .send({ nome: '' })
 
         expect(output.statusCode).toEqual(StatusCodes.BAD_REQUEST)
@@ -64,6 +94,7 @@ describe('Loja - Update', () => {
     it('Tenta criar um registro de loja onde horario de abertura é uma string vazia', async () => {
         const output = await testServer
             .put('/store-data/1')
+            .set('Authorization', `Bearer ${accessToken}`)
             .send({ horario_abertura: '' })
 
         expect(output.statusCode).toEqual(StatusCodes.BAD_REQUEST)
@@ -73,6 +104,7 @@ describe('Loja - Update', () => {
     it('Tenta criar um registro de loja onde horario de fechamento é uma string vazia', async () => {
         const output = await testServer
             .put('/store-data/1')
+            .set('Authorization', `Bearer ${accessToken}`)
             .send({ horario_fechamento: '' })
 
         expect(output.statusCode).toEqual(StatusCodes.BAD_REQUEST)
@@ -82,6 +114,8 @@ describe('Loja - Update', () => {
     it('Tenta criar um registro de loja com um ID inexistente' , async () => {
         const output = await testServer
             .put('/store-data/99999')
+            .set('Authorization', `Bearer ${accessToken}`)
+
 
         expect(output.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR)
         expect(output.body).toHaveProperty('errors.default')
