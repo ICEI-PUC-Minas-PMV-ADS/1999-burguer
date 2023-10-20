a12f82eb9884efacb1e338d3d8b56fbf41076a92
GuilhermeCmkz <guilherme_costa@outlook.com>
2023-10-20 15:16:23 -0300

    feat: implementation of order history

--- src/frontend/src/app/modules/pedidos/pages/historico-pedidos/historico-pedidos.component.ts
+++ src/frontend/src/app/modules/pedidos/pages/historico-pedidos/historico-pedidos.component.ts
@@ -5,6 +5,42 @@ import { ApiService } from 'src/app/common/services/api.service';
     templateUrl: './historico-pedidos.component.html',
     styleUrls: ['./historico-pedidos.component.scss']
 })
+export class HistoricoPedidosComponent {
+    pedidos = [
+      {
+        cliente: 'João Silva',
+        endereco: 'Rua ABC, 123',
+        telefone: '(31) 97555-5555',
+        ticket: '12345',
+        pedido: 'Hambúrguer duplo com queijo, batatas fritas, refrigerante',
+        valor: 20.50,
+        status: 'Concluído',
+        data: '2023-10-16'
+      },
+      {
+        cliente: 'Maria Souza',
+        endereco: 'Av. XYZ, 456',
+        telefone: '(31) 98555-7777',
+        ticket: '12346',
+        pedido: 'Hambúrguer simples, milkshake de chocolate',
+        valor: 15.00,
+        status: 'Concluído',
+        data: '2023-10-15'
+      },
+      {
+        cliente: 'Maria Souza',
+        endereco: 'Av. XYZ, 456',
+        telefone: '(31) 93555-7777',
+        ticket: '12347',
+        pedido: 'Hambúrguer simples, milkshake de chocolate',
+        valor: 15.00,
+        status: 'Concluído',
+        data: '2023-10-15'
+      },
+      // Adicione mais pedidos conforme necessário
+    ];
+  }
+/*
 export class HistoricoPedidosComponent implements OnInit{
 
     constructor(private apiService: ApiService) { }
@@ -12,4 +48,4 @@ export class HistoricoPedidosComponent implements OnInit{
     ngOnInit(): void {
 
     }
-}
+}*/
