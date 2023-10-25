817df783ae275b639ab6d922b387bf4524da571e
Gabriel Puddo <gabriel@regexsolutions.com.br>
2023-10-25 10:20:14 -0300

    fix header e melhorando um pouco o visual

--- src/frontend/src/app/modules/produtos/produtos.service.ts
+++ src/frontend/src/app/modules/produtos/produtos.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { Observable } from 'rxjs';
 import { ApiService } from "src/app/common/services/api.service";
 
 @Injectable()
@@ -6,4 +7,11 @@ export class ProdutosService {
     constructor(
         private _api: ApiService
     ) { }
+
+    getListaProdutos(dados: any): Observable<any> {
+
+        return this._api.crudGet('', dados, true);
+
+    }
+
 }
